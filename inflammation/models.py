b4c76c640830257cd8af5a9b12bdfd2f6bc63a59
Adam <admfisher@live.com>
2025-10-17 13:31:18 +0100

    Add test cases

--- inflammation/models.py
+++ inflammation/models.py
@@ -10,7 +10,7 @@ and each column represents a single day across all patients.
 import numpy as np
 
 
-def load_csv(filename):  
+def load_csv(filename):
     """Load a Numpy array from a CSV
 
     :param filename: Filename of CSV to load
@@ -20,8 +20,9 @@ def load_csv(filename):
 
 def daily_mean(data):
     """Calculate the daily mean of a 2D inflammation data array.
-    
-    :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+
+    :param data: A 2D data array with inflammation data 
+    (each row contains measurements for a single patient across all days).
     :returns: An array of mean values of measurements for each day.
     """
     return np.mean(data, axis=0)
@@ -29,8 +30,9 @@ def daily_mean(data):
 
 def daily_max(data):
     """Calculate the daily max of a 2D inflammation data array.
-    
-    :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+
+    :param data: A 2D data array with inflammation data 
+    (each row contains measurements for a single patient across all days).
     :returns: An array of maximum values of measurements for each day.
     """
     return np.max(data, axis=0)
@@ -38,9 +40,31 @@ def daily_max(data):
 
 def daily_min(data):
     """Calculate the daily min of a 2D inflammation data array.
-    
-    :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+
+    :param data: A 2D data array with inflammation data 
+    (each row contains measurements for a single patient across all days).
     :returns: An array of minimum values of measurements for each day.
     """
     return np.min(data, axis=0)
 
+
+def patient_normalise(data):
+    """
+    Normalise patient data from a 2D inflammation data array.
+
+    NaN values are ignored, and normalised to 0.
+
+    Negative values are rounded to 0.
+    """
+    if np.any(data < 0):
+        raise ValueError('Inflammation values should not be negative')
+    if type(data) != type(np.ndarray):
+        raise TypeError('Data input should be ndarray')
+    if len(data.shape()) != 2:
+        raise ValueError("Inflamation array should be 2D")
+    max = np.nanmax(data, axis=1)
+    with np.errstate(invalid='ignore', divide='ignore'):
+        normalised = data / max[:, np.newaxis]
+    normalised[np.isnan(normalised)] = 0
+    normalised[normalised < 0] = 0
+    return normalised
--- tests/test_models.py
+++ tests/test_models.py
@@ -5,7 +5,7 @@ from inflammation.models import daily_min
 import numpy as np
 import numpy.testing as npt
 
-from inflammation.models import daily_mean, daily_max, daily_min
+from inflammation.models import daily_mean, daily_max, daily_min, patient_normalise
 
 
 @pytest.mark.parametrize(
@@ -46,3 +46,63 @@ def test_daily_min_string():
 
     with pytest.raises(TypeError):
         error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
+
+
+@pytest.mark.parametrize(
+    "test, expected,expect_raises ",
+    [
+        (
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+            None
+        ),
+        (
+            [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
+            [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
+            None
+        ),
+        (
+            [[float('nan'), 1, 1], [1, 1, 1], [1, 1, 1]],
+            [[0, 1, 1], [1, 1, 1], [1, 1, 1]],
+            None
+        ),
+        (
+            [[1, 2, 3], [4, 5, float('nan')], [7, 8, 9]],
+            [[0.33, 0.67, 1], [0.8, 1, 0], [0.78, 0.89, 1]],
+            None
+        ),
+        (
+            [[-1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            ValueError,
+        ),
+        (
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            None,
+        ),
+        (
+            'hello',
+            None,
+            TypeError,
+        ),
+        (
+            3,
+            None,
+            TypeError,
+        ),
+        (
+            [4, 5, 6],
+            None,
+            ValueError,
+        ),
+    ])
+def test_patient_normalise(test, expected, expect_raises):
+    """Test normalisation works for arrays of one and positive integers."""
+
+    if expect_raises is not None:
+        with pytest.raises(expect_raises):
+            patient_normalise(np.array(test))
+    else:
+        result = patient_normalise(np.array(test))
+        npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
